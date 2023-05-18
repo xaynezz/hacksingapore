d12a45f3a77f8225e2013f95fd48bb432bdd5b97
Jeff Tan <77041483+Arcyonix@users.noreply.github.com>
2023-05-18 15:21:24 +0800

    updated the routing files
    to make sure the chatbot saves previous prompts

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -1,32 +1,42 @@
 import axios, { AxiosResponse } from 'axios';
-import { NextApiRequest, NextApiResponse } from 'next';
-const OPENAI = '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'
+const OPENAI = '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';
 import { NextResponse } from "next/server";
 
+// Define a variable to store the conversation history
+let conversationHistory = [{ role: 'user', content: 'You will be acting as  gardey , the AI chatbot for a mobile application focused on gardening and plants. Your purpose is to assist users in their gardening journey, providing helpful information, tips, and answering questions. You should engage in a helpful and informative dialogue, offering suggestions, troubleshooting techniques, and relevant plant care instructions to ensure a successful gardening experience.' }];
 
 /* Sends a POST request to OPENAI to answer the user's question */
 /* Returns the response data as the answer */
-
 export async function POST(request: Request) {
-      const body  = await request.json()
-      console.log(body);
-      const data = {
-        model: 'gpt-3.5-turbo',
-        messages: [{ "role": "user", "content": body.data }], };
+  const body = await request.json();
+  console.log(body);
 
-      const url = 'https://api.openai.com/v1/chat/completions';
-      const headers = {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${OPENAI}`,
-      };
-    try {
-      const response: AxiosResponse = await axios.post(url, data, { headers:headers });
-      console.log(response.data);
-      return NextResponse.json(response.data)
+  // Add the user's message to the conversation history
+  conversationHistory.push({
+    role: 'user',
+    content: body.data
+  });
 
-      ;
-    }  catch (error: any) {
-      console.error(`Error: ${error.message}`);
-    }
+  // Prepare the data for the API request
+  const data = {
+    model: 'gpt-3.5-turbo',
+    messages: conversationHistory
+  };
+
+  const url = 'https://api.openai.com/v1/chat/completions';
+  const headers = {
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${OPENAI}`,
+  };
+
+  try {
+    const response: AxiosResponse = await axios.post(url, data, { headers: headers });
+    console.log(response.data);
+    console.log(conversationHistory);
+
+
+    return NextResponse.json(response.data);
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
   }
-  
+}
