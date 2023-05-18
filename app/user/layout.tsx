cc821046ab27ff534adbf7bfd0c30a3c51b66a85
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-05-19 01:40:07 +0800

    Change to Supabase Auth

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { AiFillCamera } from 'react-icons/ai';
-import { GardenContextProvider } from "../context/gardenContext";
+
 
 export default function UserLayout({
     children,
@@ -11,7 +11,7 @@ export default function UserLayout({
     children: React.ReactNode;
 }) {
     return (
-        <GardenContextProvider>
+        
             <main className="flex min-h-screen flex-col items-center justify-between">
                 <Header title={"Garden"} icon={<AiFillCamera />} />
                 <div className="h-[calc(100vh-6.5rem)] w-full overflow-auto">
@@ -19,6 +19,5 @@ export default function UserLayout({
                 </div>
                 <Footer />
             </main>
-        </GardenContextProvider>
     );
 }
