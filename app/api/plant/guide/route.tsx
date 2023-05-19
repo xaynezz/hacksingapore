da24c47e4b525d25b1305a2b5b0d7b8ef8b85d89
Han <77002201+harpervin@users.noreply.github.com>
2023-05-19 15:43:02 +0800

    added garden page styling, added guide care info

--- app/api/plant/guide/route.tsx
+++ app/api/plant/guide/route.tsx
@@ -1,7 +1,5 @@
 
 import axios from "axios";
-const util = require('util');
-
 import { NextResponse } from "next/server";
 
 /* Sends a GET request to PERENUAL to get details of the plant based on ID */
@@ -9,18 +7,14 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     const { plant_id } = await request.json();
     console.log('guide route',plant_id)
-    await axios.get(
+    const response = await axios.get(
         `https://perenual.com/api/species-care-guide-list?key=${process.env.PERENUAL}&species_id=${plant_id}`
-    ).then(response => {
-        const circularStructure = response.data;
-        const flatObject = util.inspect(circularStructure);
-        //console.log(flatObject)
+    )
+    const res = response.data;
+    const section = res.data[0].section;
+    console.log('guide route reponse:',section)
 
-        // currently cannot return json object, need to fix it so can access section data[0].section[0]
-        return flatObject;
-        // You can now return or render this flatObject to your Next.js page
-    })
-    .catch(error => {
-        console.error(error);
-    });
+    //return section
+    return NextResponse.json(section)
+
 }
