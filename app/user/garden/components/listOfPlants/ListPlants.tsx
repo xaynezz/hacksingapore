2661de0275bd2b2c15bd53d0c0c214bf9bdff279
Han <77002201+harpervin@users.noreply.github.com>
2023-05-21 02:50:51 +0800

    added ui components for garden page, plant count, no of days, welcome page

--- app/user/garden/components/listOfPlants/ListPlants.tsx
+++ app/user/garden/components/listOfPlants/ListPlants.tsx
@@ -10,7 +10,7 @@ export default function ListPlants({ arrayOfUserPLants }: Props) {
     return (
         <div className="p-4">
 
-            <h1 className="text-xl text-center text-green-500 font-semibold mb-4 pt-40">A Peek into My Plant Collection</h1>
+            <h1 className="text-xl text-center text-green-500 font-semibold mb-4">A Peek into My Plant Collection</h1>
             <div className="grid grid-cols-2 gap-4">
                 {arrayOfUserPLants.map((plant, index) => (
                     <div key={index} className="p-4 border border-gray-200 rounded-lg">
