3ec45758d5a00cf71281b847f613edf21251ea4b
Han <77002201+harpervin@users.noreply.github.com>
2023-05-21 23:43:13 +0800

    cleaned code

--- app/user/garden/[id]/[dbid]/page.tsx
+++ app/user/garden/[id]/[dbid]/page.tsx
@@ -160,19 +160,6 @@ const GardenPlantPage = ({ params }: { params: { id: string, dbid: string } }) =
 
 
                 <div className="relative isolate overflow-hidden bg-white px-6 sm:py-16 lg:overflow-visible lg:px-0">
-                  {/* <div className="absolute inset-0 -z-10 overflow-hidden">
-                    <svg className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
-                      <defs>
-                        <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-                          <path d="M100 200V.5M.5 .5H200" fill="none" />
-                        </pattern>
-                      </defs>
-                      <svg x="50%" y="-1" className="overflow-visible fill-gray-50">
-                        <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
-                      </svg>
-                      <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-                    </svg>
-                  </div> */}
                   <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-3 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
                     
                     <div className="pt-2">
