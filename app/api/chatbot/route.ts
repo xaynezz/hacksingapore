1a7e4581fe091b56168714be5cb794b787f73d09
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-05-23 03:09:38 +0800

    setup env

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -1,49 +1,56 @@
-import axios, { AxiosResponse } from 'axios';
-const OPENAI = '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';
+import axios, { AxiosResponse } from "axios";
+const OPENAI = process.env.NEXT_PUBLIC_OPENAI_APIKEY;
 import { NextResponse } from "next/server";
 
 // Define a variable to store the conversation history
-let conversationHistory = [{ role: 'user', content: 'You will be acting as gardey , the AI chatbot for a mobile application focused on gardening and plants. Your purpose is to assist users in their gardening journey, providing helpful information, tips, and answering questions. You should engage in a helpful and informative dialogue, offering suggestions, troubleshooting techniques, and relevant plant care instructions and also cooking instructions for recipes to ensure a successful gardening experience.' }];
+let conversationHistory = [
+    {
+        role: "user",
+        content:
+            "You will be acting as gardey , the AI chatbot for a mobile application focused on gardening and plants. Your purpose is to assist users in their gardening journey, providing helpful information, tips, and answering questions. You should engage in a helpful and informative dialogue, offering suggestions, troubleshooting techniques, and relevant plant care instructions and also cooking instructions for recipes to ensure a successful gardening experience.",
+    },
+];
 
 /* Sends a POST request to OPENAI to answer the user's question */
 /* Returns the response data as the answer */
 export async function POST(request: Request) {
-  const body = await request.json();
-  console.log('body:',body.data);
+    const body = await request.json();
+    console.log("body:", body.data);
 
-// add an empty one to stop the API to answer the last 2 queries (debug the issue with the weird response.)
+    // add an empty one to stop the API to answer the last 2 queries (debug the issue with the weird response.)
 
-  conversationHistory.push({
-    role: 'user',
-    content: ''
-  });
+    conversationHistory.push({
+        role: "user",
+        content: "",
+    });
 
-  // Add the user's message to the conversation history
-  conversationHistory.push({
-    role: 'user',
-    content: body.data
-  });
+    // Add the user's message to the conversation history
+    conversationHistory.push({
+        role: "user",
+        content: body.data,
+    });
 
-  // Prepare the data for the API request
-  const data = {
-    model: 'gpt-3.5-turbo',
-    messages: conversationHistory
-  };
+    // Prepare the data for the API request
+    const data = {
+        model: "gpt-3.5-turbo",
+        messages: conversationHistory,
+    };
 
-  const url = 'https://api.openai.com/v1/chat/completions';
-  const headers = {
-    'Content-type': 'application/json',
-    Authorization: `Bearer ${OPENAI}`,
-  };
+    const url = "https://api.openai.com/v1/chat/completions";
+    const headers = {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${OPENAI}`,
+    };
 
-  try {
-    const response: AxiosResponse = await axios.post(url, data, { headers: headers });
-    console.log('response data', response.data);
-    //console.log('convo hist', conversationHistory);
+    try {
+        const response: AxiosResponse = await axios.post(url, data, {
+            headers: headers,
+        });
+        console.log("response data", response.data);
+        //console.log('convo hist', conversationHistory);
 
-
-    return NextResponse.json(response.data);
-  } catch (error: any) {
-    console.error(`Error: ${error.message}`);
-  }
+        return NextResponse.json(response.data);
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`);
+    }
 }
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://jlkjygdllpgjekvnkdsa.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impsa2p5Z2RsbHBnamVrdm5rZHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MDA2ODEsImV4cCI6MTk5OTk3NjY4MX0.I_BGKVQ0LgeHgdHxnQRQR_YI05MA7bkDudBgBzSGQCE')
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
