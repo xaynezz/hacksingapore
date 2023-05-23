600e1df452ec7f98ec818be38ec50fc312c844c6
Jeff <jefftan913@gmail.com>
2023-05-23 15:10:59 +0800

    edited landing page styling

--- app/landingcomponents/Bottomlead.tsx
+++ app/landingcomponents/Bottomlead.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 
-const BottomLead = ({actionText, description, mainActionText, extraActionText}: any) => {
-  return (
-    <div className="flex flex-col justify-center h-screen bg-green-800 lg:pt-24 lg:pb-14 lg:bg-cover lg:h-full lg:bg-action">
-      <p className="p-5 text-5xl font-bold leading-normal text-center text-white lg:text-4xl lg:pt-10">{actionText}</p>
-      <p className="pl-12 pr-12 text-xl leading-8 text-center text-gray-300 lg:text-gray-200 lg:font-medium">{description}</p>
-      <div className="flex flex-col flex-wrap items-center justify-center w-full h-48 lg:flex-row lg:pt-1">
-        <a href="/login"
-          className="pt-3 pb-3 pl-8 pr-8 text-2xl font-semibold text-center text-white transition-all bg-orange-600 shadow-2xl lg:mt-0 rounded-xl hover:bg-orange-700 focus:outline-none ring-2 ring-orange-600 "
-        >
-          {mainActionText}
-        </a>
-      </div>
-    </div>
-  );
+const BottomLead = ({
+    actionText,
+    description,
+    mainActionText,
+    extraActionText,
+}: any) => {
+    return (
+        <div className="lg:bg-action flex h-screen flex-col justify-center bg-green-600 lg:h-full lg:bg-cover lg:pb-14 lg:pt-24">
+            <p className="p-5 text-center text-5xl font-bold leading-normal text-white lg:pt-10 lg:text-4xl">
+                {actionText}
+            </p>
+            <p className="pl-12 pr-12 text-center text-xl leading-8 text-gray-50 lg:font-medium ">
+                {description}
+            </p>
+            <div className="flex h-48 w-full flex-col flex-wrap items-center justify-center lg:flex-row lg:pt-1">
+                <a
+                    href="/login"
+                    className="rounded-xl bg-secondarydark-500 pb-3 pl-8 pr-8 pt-3 text-center text-2xl font-semibold text-white shadow-2xl ring-2 ring-secondarydark-600 transition-all hover:bg-secondarydark-600 focus:outline-none active:bg-secondarydark-500 lg:mt-0 "
+                >
+                    {mainActionText}
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default BottomLead;
--- app/landingcomponents/Hero.tsx
+++ app/landingcomponents/Hero.tsx
@@ -1,45 +1,50 @@
 import React from "react";
 import Image from "next/image";
-const Hero = ({appType, tagLine, description, mainActionText, extraActionText}: any) => {
-  return (
-    <div id="product">
-      <div style={{textShadow:'0px 1px 1px gray'}} className="flex flex-col items-center justify-start font-sans min-h-50 bg-gray-50 lg:pt-10 lg:pb-20 lg:bg-hero lg:bg-cover">
-        <div>
-
-
-          <p className="p-3 pt-12 text-lg font-bold text-gray-500 lg:text-gray-300">
-          <Image
-                className="mb-10 w-72 text-3xl font-bold text-white"
-                src="/ediplants.png"
-                width={1000}
-                height={1000}
-                alt="logo"
-            ></Image>
-          </p>
+const Hero = ({
+    appType,
+    tagLine,
+    description,
+    mainActionText,
+    extraActionText,
+}: any) => {
+    return (
+        <div id="product">
+            <div
+                style={{ textShadow: "0px 1px 1px gray" }}
+                className="min-h-50 lg:bg-hero flex h-screen flex-col items-center justify-start bg-green-600 font-sans lg:bg-cover lg:pb-20 lg:pt-10"
+            >
+                <div>
+                    <p className="p-3 pt-12 text-lg font-bold text-gray-500 lg:text-gray-300">
+                        <Image
+                            className="mb-10 w-72 text-3xl font-bold text-white"
+                            src="/ediplants.png"
+                            width={1000}
+                            height={1000}
+                            alt="logo"
+                        ></Image>
+                    </p>
+                </div>
+                <div>
+                    <p className=" text-center text-4xl font-bold text-gray-50 lg:mx-auto lg:w-4/6 lg:text-5xl lg:text-gray-100">
+                        {tagLine}
+                    </p>
+                </div>
+                <div>
+                    <p className="p-2 pt-6 text-center font-sans text-xl leading-10 text-gray-50 ">
+                        {description}
+                    </p>
+                </div>
+                <div className="lg:w-90 relative z-50 flex h-48 flex-col items-center justify-between pt-7 lg:flex-row lg:justify-between lg:space-x-8 lg:pt-0">
+                    <a
+                        href="/login"
+                        className="rounded-full bg-secondarydark-500 pb-3 pl-12 pr-12 pt-3 text-center text-2xl font-semibold text-white shadow-2xl ring-4 ring-secondarydark-600 transition-all hover:bg-secondarydark-600 focus:outline-none active:bg-secondarydark-600 lg:ml-5 lg:font-medium lg:ring-2 "
+                    >
+                        {mainActionText}
+                    </a>
+                </div>
+            </div>
         </div>
-        <div>
-
-          <p className=" text-4xl font-bold text-center text-green-600 lg:mx-auto lg:w-4/6 lg:text-5xl lg:text-gray-100">
-            {tagLine}
-          </p>
-        </div>
-        <div>
-          <p className="p-2 pt-6 font-sans text-xl leading-10 text-center text-gray-500 lg:text-gray-200">
-            {description}
-          </p>
-        </div>
-        <div className="relative z-50 flex flex-col items-center justify-between h-48 lg:space-x-8 pt-7 lg:pt-0 lg:flex-row lg:justify-between lg:w-90">
-          <a href="/login"
-            className="pt-3 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all bg-orange-600 rounded-full shadow-2xl lg:ml-5 hover:bg-orange-700 focus:outline-none ring-4 ring-orange-600 lg:ring-2 lg:font-medium "
-          >
-            {mainActionText}
-          </a>
-        </div>
-        
-      </div>
-      
-    </div>
-  );
+    );
 };
 
 export default Hero;
